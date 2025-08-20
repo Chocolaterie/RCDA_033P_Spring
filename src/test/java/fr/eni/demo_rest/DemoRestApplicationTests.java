a26869d6e18023ce282ada77598e4a13dc4a641d
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2025-08-20 14:30:40 +0200

    Exemple activer mock dans tests

--- src/test/java/fr/eni/demo_rest/DemoRestApplicationTests.java
+++ src/test/java/fr/eni/demo_rest/DemoRestApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("mock")
 class DemoRestApplicationTests {
 
     @Autowired
